9b51ba656ba6e02ccb78264d64bd0307082e0c04
liuxianglin <lewislinlin@gmail.com>
2018-11-07 10:50:35 +0800

    add rspec

--- README.md
+++ README.md
@@ -24,6 +24,12 @@ Or install it yourself as:
 
 TODO: Write usage instructions here
 
+#### create client
+
+    $ client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret, endpoint)
+
+default endpoint: nlp.cn-shanghai.aliyuncs.com
+
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
--- spec/aliyun/nlp_api_spec.rb
+++ spec/aliyun/nlp_api_spec.rb
@@ -3,7 +3,21 @@ RSpec.describe Aliyun::NlpApi do
     expect(Aliyun::NlpApi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "shoud create with access_key_id, access_key_secret" do
+    access_key_id = "test_access_key_id"
+    access_key_secret = "test_access_key_secret"
+    client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret)
+    expect(client.access_key_id).to eq(access_key_id)
+    expect(client.access_key_secret).to eq(access_key_secret)
+    expect(client.host).to eq("nlp.cn-shanghai.aliyuncs.com")
+  end
+
+  it "shoud create with access_key_id, access_key_secret" do
+    access_key_id = "test_access_key_id"
+    access_key_secret = "test_access_key_secret"
+    client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret)
+    expect(client.access_key_id).to eq(access_key_id)
+    expect(client.access_key_secret).to eq(access_key_secret)
+    expect(client.host).to eq("nlp.cn-shanghai.aliyuncs.com")
   end
 end
