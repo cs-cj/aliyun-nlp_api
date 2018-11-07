4439ca0d3ecd49d271aea14225aec5e9ce24389d
liuxianglin <lewislinlin@gmail.com>
2018-11-07 10:56:17 +0800

    fix rspec

--- spec/aliyun/nlp_api_spec.rb
+++ spec/aliyun/nlp_api_spec.rb
@@ -7,17 +7,16 @@ RSpec.describe Aliyun::NlpApi do
     access_key_id = "test_access_key_id"
     access_key_secret = "test_access_key_secret"
     client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret)
-    expect(client.access_key_id).to eq(access_key_id)
-    expect(client.access_key_secret).to eq(access_key_secret)
+    expect(client.ak_id).to eq(access_key_id)
+    expect(client.ak_secret).to eq(access_key_secret)
     expect(client.host).to eq("nlp.cn-shanghai.aliyuncs.com")
   end
 
-  it "shoud create with access_key_id, access_key_secret" do
+  it "shoud create with custom host" do
     access_key_id = "test_access_key_id"
     access_key_secret = "test_access_key_secret"
-    client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret)
-    expect(client.access_key_id).to eq(access_key_id)
-    expect(client.access_key_secret).to eq(access_key_secret)
-    expect(client.host).to eq("nlp.cn-shanghai.aliyuncs.com")
+    client = Aliyun::NlpApi::Client.new(access_key_id,access_key_secret, "some.com")
+
+    expect(client.host).to eq("some.com")
   end
 end
